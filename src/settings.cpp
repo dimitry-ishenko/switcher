8343dac21325d629a185ed9d74981dcc746ce4bd
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2021-06-23 11:50:50 -0400

    Add new settings class
    It represents current proxy settings.

---
+++ src/settings.cpp
@@ -0,0 +1,41 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2021 Dimitry Ishenko
+// Contact: dimitry (dot) ishenko (at) (gee) mail (dot) com
+//
+// Distributed under the GNU GPL license. See the LICENSE.md file for details.
+
+////////////////////////////////////////////////////////////////////////////////
+#include "settings.hpp"
+#include <QVariant>
+
+////////////////////////////////////////////////////////////////////////////////
+settings::settings(QObject* parent) : QObject { parent }
+{
+    for(auto const& type : proxy::types)
+    {
+        auto gs = new QGSettings { proxy::schema_id + "." + type };
+        types.emplace(type, gs);
+
+        connect(gs, &QGSettings::changed, this, &settings::changed);
+    }
+    connect(&proxy, &QGSettings::changed, this, &settings::changed);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+settings::entry settings::to_entry()
+{
+    using namespace proxy;
+
+    entry e;
+    e.mode = proxy.get(mode).toString();
+    e.autoconfig_url = proxy.get(autoconfig_url).toString();
+    e.ignore_hosts = proxy.get(ignore_hosts).toString();
+
+    for(auto const& [ type, gs ] : types)
+    {
+        entry::uri uri { gs->get(host).toString(), gs->get(port).toInt(0) };
+        if(uri.host.size() && uri.port > 0) e.types[type] = std::move(uri);
+    }
+
+    return e;
+}
---
+++ src/settings.hpp
@@ -0,0 +1,43 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2021 Dimitry Ishenko
+// Contact: dimitry (dot) ishenko (at) (gee) mail (dot) com
+//
+// Distributed under the GNU GPL license. See the LICENSE.md file for details.
+
+////////////////////////////////////////////////////////////////////////////////
+#ifndef SETTINGS_HPP
+#define SETTINGS_HPP
+
+////////////////////////////////////////////////////////////////////////////////
+#include "profile.hpp"
+#include "proxy.hpp"
+
+#include <QGSettings>
+#include <QObject>
+
+#include <map>
+#include <memory>
+
+////////////////////////////////////////////////////////////////////////////////
+class settings : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit settings(QObject* parent = nullptr);
+
+    using entry = profile::entry;
+    entry to_entry();
+
+signals:
+    void changed();
+
+private:
+    QGSettings proxy { proxy::schema_id };
+
+    using type = QString;
+    std::map<type, std::unique_ptr<QGSettings>> types;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+#endif
